57ddd6d8e65fda1dbdba5231fe6224203e38b8a5
Ramil-Karipov <omil8@yandex.ru>
2024-01-22 14:35:28 +0500

    Finish1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        EmployeeBook employeeBook = new EmployeeBook();
+        employeeBook.addContact("Новосельцева Виктория Викторовна", 1, 33_000);
+        employeeBook.addContact("Константинов Ефим Вячеславович ", 2, 50_000);
+        employeeBook.addContact("Копадзе Давид Михайлович ", 3, 70_000);
+        employeeBook.printAll();
+        System.out.println();
+        employeeBook.update("Константинов Ефим Вячеславович ", 2, 100_000);
+        try {
+            employeeBook.update("Константинова Виктория Викторовна", 1, 70_000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        employeeBook.printAll();
+        System.out.println();
+
+        employeeBook.delete(2);
+        employeeBook.delete("Копадзе");
+        employeeBook.printAll();
 
     }
 }
